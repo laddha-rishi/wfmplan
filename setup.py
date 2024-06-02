4e76648a7380a7472804a87e23744063b402e803
Rishi Laddha <laddha.rishi@gmail.com>
2024-06-02 12:52:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="wfmplan",
-    version="0.2.2",
+    version="0.2.3",
     description="Optimization library for workforce management planning",
     long_description=long_description,
     long_description_content_type='text/markdown',
